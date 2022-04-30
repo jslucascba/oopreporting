f3daac981bc365d99467b487cd0ed356708df693
Jeff Lucas <45663491+jslucascba@users.noreply.github.com>
2022-04-30 10:03:33 -0500

    init commit

---
+++ Menu.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace oopprojectsec1
+{
+    public class Menu
+    {
+
+        public int MainMenu()
+        {
+            System.Console.WriteLine("Enter 1 for Search\nEnter 2 for Reports\nEnter 3 to exit");
+            string userInput = Console.ReadLine();
+            while (userInput != "1" && userInput != "2" && userInput != "3")
+            {
+                System.Console.WriteLine("Invalid choice.  Please enter a valid option");
+                System.Console.WriteLine("Enter 1 for Search\nEnter 2 for Reports\nEnter 3 to exit");
+                userInput = Console.ReadLine();
+            }
+
+            return int.Parse(userInput);
+        }
+
+        public int ReportMenu()
+        {
+            System.Console.WriteLine("Enter 1 for GPA Report\nEnter 2 for Hours by Year Report");
+            System.Console.WriteLine("Enter 3 for Excess Credit Report\n Enter 4 to return");
+            string userInput = Console.ReadLine();
+            while (userInput != "1" && userInput != "2" && userInput != "3" && userInput != "4")
+            {
+                System.Console.WriteLine("Invalid choice.  Please enter a valid option");
+                System.Console.WriteLine("Enter 1 for GPA Report\nEnter 2 for Hours by Year Report");
+                System.Console.WriteLine("Enter 3 for Excess Credit Report\n Enter 4 to return");
+                userInput = Console.ReadLine();
+            }
+
+            return int.Parse(userInput);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace oopprojectsec1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            StudentFile myDataHandler = new StudentFile("students.txt");
+            Student[] myStudents = myDataHandler.GetAllStudents();
+            StudentReports reports = new StudentReports(myStudents);
+            Utility myUtility = new Utility(myStudents);
+            myUtility.Sort();
+
+            Menu menu = new Menu();
+            menu.MainMenu();
+
+            menu.ReportMenu();
+
+            // Console.WriteLine(" After sort -------------------------");
+
+            // reports.PrintAllStudents();
+
+            // string searchVal = "Makayla Townsend";
+
+            // int found = myUtility.Search(searchVal);
+            // if(found != -1)
+            // {
+            //     Console.WriteLine("What should credit hour be");
+            //     int updatedHours = int.Parse(Console.ReadLine());
+            //     myStudents[found].SetCreditHours(updatedHours); 
+            // }
+
+            // myDataHandler.Save(myStudents);
+
+            //reports.ExcessGpa();
+
+            //reports.HoursByYear();
+        }
+    }
+}
